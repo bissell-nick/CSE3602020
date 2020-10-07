6d13c0fdf7bc4b5ddc8a23809d361cc7df9445c4
Nicholas Bissell <Nicholas Bissell@DESKTOP-2DTNN3H>
2020-10-07 09:04:44 -0700

    Implemented methods described by assignment

--- Assignment1/src/cse360assignment02/AddingMachine.java
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,34 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = "0";
   }
   
   public int getTotal () {//returns current total
-    return 0;
+    return total;
   }
   
   public void add (int value) { //adds parameter to total
+	  total = total + value;
+	 history = history.concat(" + " + value);
   }
 
   public void subtract (int value) { //subtracts paramater from total
+	  total = total - value;
+	  history = history.concat(" - " + value);
   }
 
   public String toString () {//returns history of transactions
-    return "";
+    return history;
   }
 
   public void clear() {//clears the adding machine
+	  total = 0;
+	  history = "0";
   }
 }
+
