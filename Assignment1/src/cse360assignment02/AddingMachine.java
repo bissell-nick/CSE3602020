ff67e4cfc43568fbbeb3010ceec5ef81bfe16663
Nicholas Bissell <Nicholas Bissell@DESKTOP-2DTNN3H>
2020-10-07 08:44:00 -0700

    Commit of given psuedo code.

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {//returns current total
+    return 0;
+  }
+  
+  public void add (int value) { //adds parameter to total
+  }
+
+  public void subtract (int value) { //subtracts paramater from total
+  }
+
+  public String toString () {//returns history of transactions
+    return "";
+  }
+
+  public void clear() {//clears the adding machine
+  }
+}
